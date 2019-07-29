c41c15fa99566e19f997c2acdd5314b415576a86
Oleksii Hryviuk <petrovi4@i.ua>
2019-07-29 16:44:26 +0300

    twelfth task update

--- FL11_HW12/homework/src/app.js
+++ FL11_HW12/homework/src/app.js
@@ -1,12 +1,26 @@
 const rootNode = document.getElementById('root');
-
 let todoItems = [];
 
-if (localStorage.getItem('todoItems') === null) {
-	localStorage.setItem('todoItems', JSON.stringify(todoItems));
-} else {
-	todoItems = JSON.parse(localStorage.getItem('todoItems'));
+let storage = {
+	init: () => localStorage.getItem('todoItems') === null ? storage.set() : storage.get(),
+	set: () => localStorage.setItem('todoItems', JSON.stringify(todoItems)),
+	get: () => {
+ todoItems = JSON.parse(localStorage.getItem('todoItems')) 
 }
+}
+storage.init();
+
+let routes = {
+	'/': mainPage,
+	'#/add-task': addModifyPage,
+	'#/modify': addModifyPage
+}
+
+function startPage() {
+	window.location.hash = '';
+	routes[window.location.pathname]();
+}
+startPage();
 
 function mainPage() {
 	rootNode.innerHTML = '';
@@ -24,22 +38,20 @@ function mainPage() {
 		list.textContent = 'TODO is empty';
 	} else {
 		let ul = document.createElement('ul');
-		todoItems.forEach(function(item) {
+		todoItems.forEach((item) => {
 			let li = document.createElement('li');
 			li.setAttribute('id', item.id);
 			let checkTask = document.createElement('span');
-			if (item.isDone) {
-				checkTask.setAttribute('class', 'checkTrue');
-			} else {
-				checkTask.setAttribute('class', 'checkFalse');
-			}
+			let checkValue;
+			item.isDone ? checkValue = 'checkTrue' : checkValue = 'checkFalse';
+			checkTask.setAttribute('class', checkValue);
 			li.appendChild(checkTask);
 			let descriptionTask = document.createElement('span');
 			descriptionTask.setAttribute('class', 'descriptionTask');
 			descriptionTask.textContent = item.description;
 			if (item.isDone) {
-				descriptionTask.style.background = 'grey';
-			}
+descriptionTask.style.background = 'grey'; 
+}
 			li.appendChild(descriptionTask);
 			let deleteTask = document.createElement('span');
 			deleteTask.setAttribute('class', 'deleteTask');
@@ -56,11 +68,9 @@ function addModifyPage(flag) {
 	rootNode.innerHTML = '';
 	let fragment = document.createDocumentFragment();
 	let h1 = document.createElement('h1');
-	if (flag) {
-		h1.textContent = 'Add task';
-	} else {
-		h1.textContent = 'Modify item';
-	}
+	let text;
+	flag ? text = 'Add task' : text = 'Modify item';
+	h1.textContent = text;
 	fragment.appendChild(h1);
 	let form = document.createElement('form');
 	let input = document.createElement('input');
@@ -68,10 +78,10 @@ function addModifyPage(flag) {
 		let position = location.hash.indexOf('item_id')+7;
 		let id = location.hash.substr(position)
 		input.setAttribute('id', id);
-		todoItems.forEach(function(item) {
+		todoItems.forEach((item) => {
 			if (item.id === +id) {
-				input.value = item.description;
-			}
+ input.value = item.description; 
+}
 		});
 	}
 	form.appendChild(input);
@@ -81,21 +91,63 @@ function addModifyPage(flag) {
 	cancel.textContent = 'Cancel';
 	fragment.appendChild(cancel);
 	let saveModify = document.createElement('button');
-	if (flag) {
-		saveModify.setAttribute('id', 'save');
-	} else {
-		saveModify.setAttribute('id', 'modify');
-	}
+	let activity;
+	flag ? activity = 'save' : activity = 'modify';
+	saveModify.setAttribute('id', activity);
 	saveModify.textContent = 'Save changes';
 	fragment.appendChild(saveModify);
 	rootNode.appendChild(fragment);
 }
 
+function transformData(target, flag) {
+	let unchecked = [], checked = [];
+	todoItems.map((item) => {
+		if (item.id === +target.parentNode.id) {
+ item.isDone = flag; 
+}
+		return item;
+	});
+    todoItems.forEach((item) => item.isDone ? checked.push(item) : unchecked.push(item));
+    todoItems = unchecked.concat(checked);
+}
+
+function addModifyItem(target, str) {
+	let flag = true;
+	let task = document.getElementsByTagName('input')[0];
+	todoItems.forEach((item) => {
+		if (item.description === task.value.trim()) {
+ flag = false; 
+}
+	});
+	if (flag) {
+		if (task.value.trim().length !== 0) {
+			if (str === 'save') {
+				todoItems.push({isDone: false, id: Date.now(), description: task.value});
+				transformData(target, false);
+			}
+			if (str ==='modify') {
+				todoItems = todoItems.map((item) => {
+					if(item.id === +task.id && item.description !== task.value.trim()) {
+ item.description = task.value.trim();
+}
+					return item;
+				});
+			}
+			storage.set();
+			window.location.hash = '';
+		} else {
+			alert('You can\'t save empty task!');
+		}
+	} else {
+		errorMessage('Error! You can\'t add already exist item');
+	}
+}
+
 function errorMessage(str) {
 	let hideMessage = document.querySelector('.alert');
 	if (hideMessage !== null) {
-		hideMessage.remove();
-	}
+ hideMessage.remove(); 
+}
 	let div = document.createElement('div');
 	if (navigator.userAgent.indexOf('Chrome') !== -1) {
 		div.setAttribute('class', 'alert chrome');
@@ -110,148 +162,59 @@ function errorMessage(str) {
 	div.appendChild(p);
 	rootNode.appendChild(div);
 	hideMessage = document.querySelector('.alert');
-	setTimeout(function(hideMessage) {
+	setTimeout((hideMessage) => {
 		hideMessage.remove();
 	}, 2000, hideMessage);
 }
 
-let routes = {
-	'/': mainPage,
-	'#/add-task': addModifyPage,
-	'#/modify': addModifyPage
-}
-
-window.location.hash = '';
-routes[window.location.pathname]();
-
-window.addEventListener('click', function(e) { 
+function catchActivity(e) {
 	let target = e.target;
 	if (target.getAttribute('id') === 'newTask') {
-		window.location.hash = '#/add-task';
-	}
+ window.location.hash = '#/add-task'; 
+}
 	if (target.getAttribute('id') === 'cancel') {
-		window.location.hash = '';
-	}
+ window.location.hash = '';
+}
 	if (target.getAttribute('id') === 'save') {
-		let flag = true;
-		let newTask = document.getElementsByTagName('input')[0];
-		todoItems.forEach(function(item) {
-			if (item.description === newTask.value.trim()) {
-				flag = false;
-			}
-		});
-		if (flag) {
-			if (newTask.value.trim().length !== 0) {
-				todoItems.push({isDone: false, id: Date.now(), description: newTask.value});
-				localStorage.setItem('todoItems', JSON.stringify(todoItems));
-				window.location.hash = '';
-			} else {
-				alert('You can\'t save empty task!');
-			}
-		} else {
-			errorMessage('Error! You can\'t add already exist item');
-		}
-	}
+ addModifyItem(target, 'save'); 
+}
 	if (target.getAttribute('id') === 'modify') {
-		let flag = true;
-		let changeTask = document.getElementsByTagName('input')[0];
-		todoItems.forEach(function(item) {
-			if (item.description === changeTask.value.trim()) {
-				flag = false;
-			}
-		});
-		if (flag) {
-			if (changeTask.value.trim().length !== 0) {
-				todoItems = todoItems.map(function(item) {
-					if(item.id === +changeTask.id && item.description !== changeTask.value.trim()) {
-						item.description = changeTask.value.trim();
-					}
-					return item;
-				});
-				localStorage.setItem('todoItems', JSON.stringify(todoItems));
-				window.location.hash = '';
+ addModifyItem(target, 'modify'); 
+}
+	if (target.getAttribute('class') !== null) {
+		if (target.getAttribute('class') === 'checkTrue') {
+transformData(target, false); 
+}
+		if (target.getAttribute('class') === 'checkFalse') {
+transformData(target, true); 
+}
+		if (target.getAttribute('class') === 'deleteTask') {
+ todoItems = todoItems.filter((item) => item.id !== +target.parentNode.id); 
+}
+		if (target.getAttribute('class') === 'descriptionTask') {
+			let canModify = target.parentNode.querySelector('.checkFalse');
+			if (canModify){
+				window.location.hash = `#/modify/:item_id${target.parentNode.id}`;
 			} else {
-				alert('You can\'t save empty task!');
+				errorMessage('Error! You can\'t edit already done item');
 			}
 		} else {
-			errorMessage('Error! You can\'t add already exist item');
+			storage.set();
+			storage.get();
+			mainPage();
 		}
 	}
-	if (target.getAttribute('class') === 'checkTrue') {
-		let idTask = +target.parentNode.id;
-		todoItems.map(function(item) {
-			if (item.id === idTask) {
-				item.isDone = false;
-			}
-			return item;
-		});
-		let unchecked = [];
-        let checked = [];
-        todoItems.forEach(function(item) {
-            if (item.isDone) {
-                checked.push(item);
-            } else {
-                unchecked.push(item);
-            }
-        });
-        todoItems = unchecked.concat(checked);
-		localStorage.setItem('todoItems', JSON.stringify(todoItems));
-		todoItems = JSON.parse(localStorage.getItem('todoItems'));
-		mainPage();
-	}
-	if (target.getAttribute('class') === 'checkFalse') {
-		let idTask = +target.parentNode.id;
-		todoItems.map(function(item) {
-			if (item.id === idTask) {
-				item.isDone = true;
-			}
-			return item;
-		});
-		let unchecked = [];
-        let checked = [];
-        todoItems.forEach(function(item) {
-            if (item.isDone) {
-                checked.push(item);
-            } else {
-                unchecked.push(item);
-            }
-        });
-        todoItems = unchecked.concat(checked);
-		localStorage.setItem('todoItems', JSON.stringify(todoItems));
-		todoItems = JSON.parse(localStorage.getItem('todoItems'));
-		mainPage();
-	}
-	if (target.getAttribute('class') === 'descriptionTask') {
-		let canModify = target.parentNode.querySelector('.checkFalse');
-		if (canModify){
-			let taskId = target.parentNode.id;
-			window.location.hash = `#/modify/:item_id${taskId}`;
-		} else {
-			errorMessage('Error! You can\'t edit already done item');
-		}
-	}
-	if (target.getAttribute('class') === 'deleteTask') {
-		let idTask = +target.parentNode.id;
-		todoItems = todoItems.filter(function(item) {
-			return item.id !== idTask;
-		});
-		localStorage.setItem('todoItems', JSON.stringify(todoItems));
-		todoItems = JSON.parse(localStorage.getItem('todoItems'));
-		mainPage();
-	}
-});
+}
 
-window.addEventListener('hashchange', function(e) {
+function changeHash(e) {
 	if (e.newURL.indexOf('#/add-task') !== -1) {
 		routes['#/add-task'](true);
-	}
-	if (e.newURL.indexOf('#/modify') !== -1) {
+	} else if (e.newURL.indexOf('#/modify') !== -1) {
 		routes['#/modify'](false);
-	}
-});
-
-window.onpopstate = function(event) {
-	if (event.state === null) {
+	}else {
 		routes['/']();
 	}
-};
+}
+
+window.addEventListener('click', (e) => catchActivity(e));
+window.addEventListener('hashchange', (e) => changeHash(e));
